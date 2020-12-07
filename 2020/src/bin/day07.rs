2a5045e5803af4e2703ef1ebce90d40450e24991
Carl D. Benson <carl.d.benson@gmail.com>
2020-12-07 14:40:33 -0600

    Day 7, part 2. I might come back to this and clean it up.

--- 2020/src/bin/day07.rs
+++ 2020/src/bin/day07.rs
@@ -47,6 +47,33 @@ impl FromStr for InvertedBagGraph {
     }
 }
 
+struct BagGraph {
+    map: HashMap<Bag, Vec<(usize, Bag)>>,
+}
+
+impl FromStr for BagGraph {
+    type Err = ParseError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let rule_re = Regex::new(r"(\w+ \w+) bags contain (.*)\.\n?").unwrap();
+        let bag_count_re = Regex::new(r"(\d+) (\w+ \w+) bags?").unwrap();
+
+        let mut graph = HashMap::new();
+
+        for cap in rule_re.captures_iter(s) {
+            let container = Bag::new(cap.get(1).unwrap().as_str());
+            let children_str = cap.get(2).unwrap().as_str();
+            let entry = graph.entry(container).or_insert_with(|| Vec::new());
+            for child_cap in bag_count_re.captures_iter(children_str) {
+                let child = Bag::new(child_cap.get(2).unwrap().as_str());
+                let count = child_cap.get(1).unwrap().as_str().parse().unwrap();
+                entry.push((count, child));
+            }
+        }
+
+        Ok(BagGraph { map: graph })
+    }
+}
+
 fn count_all_containing_bags(bag: &Bag, bag_graph: &InvertedBagGraph) -> usize {
     let mut to_process: HashSet<&Bag> = HashSet::new();
     let mut processed: HashSet<&Bag> = HashSet::new();
@@ -61,19 +88,33 @@ fn count_all_containing_bags(bag: &Bag, bag_graph: &InvertedBagGraph) -> usize {
     processed.len()
 }
 
+fn count_all_internal_bags(bag: &Bag, bag_graph: &BagGraph) -> usize {
+    bag_graph
+        .map
+        .get(bag)
+        .map(|l| {
+            l.iter()
+                .map(|(c, b)| c * count_all_internal_bags(b, bag_graph) + c)
+                .sum()
+        })
+        .unwrap_or(0)
+}
+
 fn part1(bag_graph: &InvertedBagGraph) -> usize {
     let shiny_gold = Bag::new("shiny gold");
     count_all_containing_bags(&shiny_gold, bag_graph)
 }
 
-fn part2(bag_graph: &InvertedBagGraph) -> usize {
-    0
+fn part2(bag_graph: &BagGraph) -> usize {
+    let shiny_gold = Bag::new("shiny gold");
+    count_all_internal_bags(&shiny_gold, bag_graph)
 }
 
 fn main() -> Result<()> {
-    let graph: InvertedBagGraph = read_file("input/day07.txt")?;
-    let result = part1(&graph);
+    let inverted_graph = read_file("input/day07.txt")?;
+    let result = part1(&inverted_graph);
     println!("part 1: {}", result);
+    let graph = read_file("input/day07.txt")?;
     let result = part2(&graph);
     println!("part 2: {}", result);
     Ok(())
