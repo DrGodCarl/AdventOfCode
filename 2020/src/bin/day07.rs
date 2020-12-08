6e8ea9a7a6493d6ebd250379b4e8b32a53ca6869
Carl D. Benson <carl.d.benson@gmail.com>
2020-12-07 21:25:45 -0600

    Day 7 refactor. Much better.

--- 2020/src/bin/day07.rs
+++ 2020/src/bin/day07.rs
@@ -8,47 +8,8 @@ use anyhow::Result;
 use regex::Regex;
 use utils::read_file;
 
-#[derive(Eq, PartialEq, Hash, Clone, Debug)]
-struct Bag {
-    color: String,
-}
-
-impl Bag {
-    fn new(color: &str) -> Self {
-        Bag {
-            color: String::from(color),
-        }
-    }
-}
-
-struct InvertedBagGraph {
-    map: HashMap<Bag, HashSet<Bag>>,
-}
-
-impl FromStr for InvertedBagGraph {
-    type Err = ParseError;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let rule_re = Regex::new(r"(\w+ \w+) bags contain (.*)\.\n?").unwrap();
-        let bag_count_re = Regex::new(r"\d+ (\w+ \w+) bags?").unwrap();
-
-        let mut graph = HashMap::new();
-
-        for cap in rule_re.captures_iter(s) {
-            let container = Bag::new(cap.get(1).unwrap().as_str());
-            let children_str = cap.get(2).unwrap().as_str();
-            for child_cap in bag_count_re.captures_iter(children_str) {
-                let child = Bag::new(child_cap.get(1).unwrap().as_str());
-                let entry = graph.entry(child).or_insert_with(|| HashSet::new());
-                entry.insert(container.clone());
-            }
-        }
-
-        Ok(InvertedBagGraph { map: graph })
-    }
-}
-
 struct BagGraph {
-    map: HashMap<Bag, Vec<(usize, Bag)>>,
+    map: HashMap<String, Vec<(usize, String)>>,
 }
 
 impl FromStr for BagGraph {
@@ -60,11 +21,11 @@ impl FromStr for BagGraph {
         let mut graph = HashMap::new();
 
         for cap in rule_re.captures_iter(s) {
-            let container = Bag::new(cap.get(1).unwrap().as_str());
+            let container = String::from(cap.get(1).unwrap().as_str());
             let children_str = cap.get(2).unwrap().as_str();
             let entry = graph.entry(container).or_insert_with(|| Vec::new());
             for child_cap in bag_count_re.captures_iter(children_str) {
-                let child = Bag::new(child_cap.get(2).unwrap().as_str());
+                let child = String::from(child_cap.get(2).unwrap().as_str());
                 let count = child_cap.get(1).unwrap().as_str().parse().unwrap();
                 entry.push((count, child));
             }
@@ -74,21 +35,48 @@ impl FromStr for BagGraph {
     }
 }
 
-fn count_all_containing_bags(bag: &Bag, bag_graph: &InvertedBagGraph) -> usize {
-    let mut to_process: HashSet<&Bag> = HashSet::new();
-    let mut processed: HashSet<&Bag> = HashSet::new();
-    let default_set = HashSet::new();
-    to_process.extend(bag_graph.map.get(bag).unwrap_or(&default_set));
-    while to_process.len() > 0 {
-        let bag = to_process.iter().take(1).map(|&i| i).collect::<Vec<_>>()[0];
-        processed.insert(bag);
-        to_process.extend(bag_graph.map.get(bag).unwrap_or(&default_set));
-        to_process.take(bag);
+impl BagGraph {
+    fn inverted(&self) -> BagGraph {
+        let mut result: HashMap<String, Vec<(usize, String)>> = HashMap::new();
+
+        for (bag, contents) in self.map.clone() {
+            for (num, inner_bag) in contents {
+                let entry = result.entry(inner_bag).or_insert_with(|| Vec::new());
+                entry.push((num, bag.clone()));
+            }
+        }
+
+        BagGraph { map: result }
     }
-    processed.len()
 }
 
-fn count_all_internal_bags(bag: &Bag, bag_graph: &BagGraph) -> usize {
+fn _count_all_containing_bags(
+    bag: &str,
+    bag_graph: &BagGraph,
+    visited: &mut HashSet<String>,
+) -> usize {
+    if visited.contains(bag) {
+        0
+    } else {
+        visited.insert(bag.to_string());
+        bag_graph
+            .map
+            .get(bag)
+            .iter()
+            .flat_map(|&containers| containers.iter())
+            .map(|(_, inner)| _count_all_containing_bags(inner, bag_graph, visited))
+            .sum::<usize>()
+            + 1
+    }
+}
+
+fn count_all_containing_bags(bag: &str, bag_graph: &BagGraph) -> usize {
+    let mut visited = HashSet::new();
+    // -1 because the recursive function counts the bag you're currently on and we don't want to count this one.
+    _count_all_containing_bags(bag, bag_graph, &mut visited) - 1
+}
+
+fn count_all_internal_bags(bag: &str, bag_graph: &BagGraph) -> usize {
     bag_graph
         .map
         .get(bag)
@@ -100,21 +88,21 @@ fn count_all_internal_bags(bag: &Bag, bag_graph: &BagGraph) -> usize {
         .unwrap_or(0)
 }
 
-fn part1(bag_graph: &InvertedBagGraph) -> usize {
-    let shiny_gold = Bag::new("shiny gold");
+fn part1(bag_graph: &BagGraph) -> usize {
+    let shiny_gold = "shiny gold";
     count_all_containing_bags(&shiny_gold, bag_graph)
 }
 
 fn part2(bag_graph: &BagGraph) -> usize {
-    let shiny_gold = Bag::new("shiny gold");
+    let shiny_gold = "shiny gold";
     count_all_internal_bags(&shiny_gold, bag_graph)
 }
 
 fn main() -> Result<()> {
-    let inverted_graph = read_file("input/day07.txt")?;
+    let graph: BagGraph = read_file("input/day07.txt")?;
+    let inverted_graph = graph.inverted();
     let result = part1(&inverted_graph);
     println!("part 1: {}", result);
-    let graph = read_file("input/day07.txt")?;
     let result = part2(&graph);
     println!("part 2: {}", result);
     Ok(())
