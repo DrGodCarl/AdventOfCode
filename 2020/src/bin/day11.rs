fda3a156d8c653cd2062b179baaef26235fcd653
Carl D. Benson <carl.d.benson@gmail.com>
2020-12-12 22:29:33 -0600

    Alright, last performance improvement to day 11. Built a graph of what chairs each chair can see rather than looking it up each time. About a 25% improvement.

--- 2020/src/bin/day11.rs
+++ 2020/src/bin/day11.rs
@@ -1,4 +1,4 @@
-use std::{cmp::min, fmt::Display, str::FromStr};
+use std::{cmp::min, collections::HashMap, fmt::Display, str::FromStr};
 
 use anyhow::Result;
 use itertools::Itertools;
@@ -17,12 +17,16 @@ enum State {
     Floor,
 }
 
+type Point = (usize, usize);
+type AdjacencyGraph = HashMap<Point, Vec<Point>>;
+
 #[derive(PartialEq, Debug, Clone)]
 struct SeatingArea {
     state: Vec<State>,
     width: usize,
     height: usize,
     changed: bool,
+    graph: AdjacencyGraph,
 }
 
 enum Mode {
@@ -31,8 +35,71 @@ enum Mode {
 }
 
 impl SeatingArea {
-    fn get_state(&self, x: usize, y: usize) -> State {
-        self.state[x + y * self.width]
+    fn get_index(&self, point: &Point) -> usize {
+        point.0 + point.1 * self.width
+    }
+
+    fn get_state(&self, point: &Point) -> State {
+        self.state[self.get_index(point)]
+    }
+
+    fn build_graph(&mut self, mode: &Mode) {
+        self.graph = (0..self.width)
+            .cartesian_product(0..self.height)
+            .filter(|p| self.get_state(p) == State::OpenChair)
+            .map(|p| (p, self.find_neighbors(&p, mode)))
+            .collect();
+    }
+
+    fn find_neighbors(&self, point: &Point, mode: &Mode) -> Vec<Point> {
+        match mode {
+            Mode::Neighbor => self.find_physical_neighbors(point),
+            Mode::Vision => self.find_visual_neighbors(point),
+        }
+    }
+
+    fn find_physical_neighbors(&self, point: &Point) -> Vec<Point> {
+        let (x, y) = point.clone();
+        let x_min = x.checked_sub(1).unwrap_or(0);
+        let x_max = min(x + 1, self.width - 1);
+        let y_min = y.checked_sub(1).unwrap_or(0);
+        let y_max = min(y + 1, self.height - 1);
+        (x_min..=x_max)
+            .cartesian_product(y_min..=y_max)
+            .filter(|coords| coords != &(x, y))
+            .filter(|p| self.get_state(p) == State::OpenChair)
+            .collect()
+    }
+
+    // vision is, in order, the cells seen when looking in a particular direction.
+    // we're looking for a chair, taken or open.
+    fn look_for_chair<I>(&self, mut vision: I) -> Option<Point>
+    where
+        I: Iterator<Item = (usize, usize)>,
+    {
+        vision.find(|p| self.get_state(p) == State::OpenChair)
+    }
+
+    fn find_visual_neighbors(&self, point: &Point) -> Vec<Point> {
+        // Each of these represents (inter)cardinal directions as coordinates.
+        // For instance, the first one is coordinates you look at when you look
+        // up, cell by cell in the seating area
+        let (x, y) = point.clone();
+        let up = self.look_for_chair((0..y).rev().map(|j| (x, j)));
+        let up_right = self.look_for_chair((x + 1..self.width).zip((0..y).rev()));
+        let right = self.look_for_chair((x + 1..self.width).map(|i| (i, y)));
+        let down_right = self.look_for_chair((x + 1..self.width).zip(y + 1..self.height));
+        let down = self.look_for_chair((y + 1..self.height).map(|j| (x, j)));
+        let down_left = self.look_for_chair((0..x).rev().zip(y + 1..self.height));
+        let left = self.look_for_chair((0..x).rev().map(|i| (i, y)));
+        let up_left = self.look_for_chair((0..x).rev().zip((0..y).rev()));
+        [
+            up, up_right, right, down_right, down, down_left, left, up_left,
+        ]
+        .iter()
+        .filter_map(|&a| a)
+        .filter(|p| self.get_state(p) == State::OpenChair)
+        .collect()
     }
 
     fn count_sitters(&self) -> usize {
@@ -42,75 +109,43 @@ impl SeatingArea {
             .count()
     }
 
-    fn neighbor_count(&self, x: usize, y: usize) -> usize {
-        let x_min = x.checked_sub(1).unwrap_or(0);
-        let x_max = min(x + 1, self.width - 1);
-        let y_min = y.checked_sub(1).unwrap_or(0);
-        let y_max = min(y + 1, self.height - 1);
-        (x_min..=x_max)
-            .cartesian_product(y_min..=y_max)
-            .filter(|coords| coords != &(x, y))
-            .map(|(i, j)| self.get_state(i, j))
+    fn count_occupied_neighbors(&self, point: &Point) -> usize {
+        self.graph
+            .get(point)
+            .iter()
+            .flat_map(|ns| ns.iter())
+            .map(|n| self.get_state(n))
             .filter(|s| s == &State::TakenChair)
             .count()
     }
 
-    // vision is, in order, the cells seen when looking in a particular direction.
-    // we're looking for a chair, taken or open.
-    fn look<I>(&self, vision: I) -> Option<State>
-    where
-        I: Iterator<Item = (usize, usize)>,
-    {
-        vision
-            .map(|(i, j)| self.get_state(i, j))
-            .find(|s| s == &State::TakenChair || s == &State::OpenChair)
-    }
-
-    fn vision_neighbor_count(&self, x: usize, y: usize) -> usize {
-        // Each of these represents (inter)cardinal directions as coordinates.
-        // For instance, the first one is coordinates you look at when you look
-        // up, cell by cell in the seating area
-        let up = self.look((0..y).rev().map(|j| (x, j)));
-        let up_right = self.look((x + 1..self.width).zip((0..y).rev()));
-        let right = self.look((x + 1..self.width).map(|i| (i, y)));
-        let down_right = self.look((x + 1..self.width).zip(y + 1..self.height));
-        let down = self.look((y + 1..self.height).map(|j| (x, j)));
-        let down_left = self.look((0..x).rev().zip(y + 1..self.height));
-        let left = self.look((0..x).rev().map(|i| (i, y)));
-        let up_left = self.look((0..x).rev().zip((0..y).rev()));
-        [
-            up, up_right, right, down_right, down, down_left, left, up_left,
-        ]
-        .iter()
-        .filter_map(|&a| a)
-        .filter(|s| s == &State::TakenChair)
-        .count()
-    }
-
     fn tick(&mut self, mode: &Mode) {
-        let counter = match mode {
-            Mode::Neighbor => Self::neighbor_count,
-            Mode::Vision => Self::vision_neighbor_count,
-        };
-        let mut changed = false;
-        let new_state = (0..self.width)
-            .cartesian_product(0..self.height)
-            .sorted_by_key(|(x, _)| *x)
-            .sorted_by_key(|(_, y)| *y)
-            .map(|(x, y)| {
-                let old_state = self.get_state(x, y);
-                let new_state = match (counter(&self, x, y), old_state, mode) {
+        if self.graph.len() == 0 {
+            self.build_graph(mode);
+        }
+        let map: Vec<(&Point, State)> = self
+            .graph
+            .keys()
+            .map(|p| (p, self.count_occupied_neighbors(p), self.get_state(p)))
+            .filter_map(|(p, c, old_state)| {
+                let new_state = match (c, old_state, mode) {
                     (0, State::OpenChair, _) => State::TakenChair,
                     (5..=8, State::TakenChair, _) => State::OpenChair,
                     (4, State::TakenChair, Mode::Neighbor) => State::OpenChair,
                     (_, state, _) => state,
                 };
-                changed = changed || new_state != old_state;
-                new_state
+                if new_state != old_state {
+                    Some((p, new_state))
+                } else {
+                    None
+                }
             })
             .collect();
-        self.changed = changed;
-        self.state = new_state;
+        self.changed = map.len() > 0;
+        for (p, s) in map {
+            let index = self.get_index(p);
+            self.state[index] = s;
+        }
     }
 }
 
@@ -132,6 +167,7 @@ impl FromStr for SeatingArea {
             width,
             height,
             changed: false,
+            graph: AdjacencyGraph::new(),
         })
     }
 }
