f2a16b72ca44163944f63193bd48be05eed44437
Carl D. Benson <carl.d.benson@gmail.com>
2020-12-14 22:59:22 -0600

    Day 14, part 2. Managed to tack on part 2 but it's pretty hacky.

--- 2020/src/bin/day14.rs
+++ 2020/src/bin/day14.rs
@@ -9,8 +9,12 @@ use utils::{read_file, InputParseError};
 
 #[derive(Copy, Clone)]
 struct Mask {
+    // v1
     and: u64,
+    // v1 & v2
     or: u64,
+    // v2
+    floating_bits: u64,
 }
 
 enum Instr {
@@ -29,7 +33,13 @@ impl FromStr for Instr {
         fn make_mask(mask: &str) -> Result<Instr, ParseIntError> {
             let and = u64::from_str_radix(mask.replace("X", "1").as_str(), 2)?;
             let or = u64::from_str_radix(mask.replace("X", "0").as_str(), 2)?;
-            Ok(Instr::Mask(Mask { and, or }))
+            let floating_bits =
+                u64::from_str_radix(mask.replace("1", "0").replace("X", "1").as_str(), 2)?;
+            Ok(Instr::Mask(Mask {
+                and,
+                or,
+                floating_bits,
+            }))
         }
         fn make_mem(addr: &str, val: &str) -> Result<Instr, ParseIntError> {
             let addr = addr.parse()?;
@@ -56,6 +66,25 @@ struct Computer {
     mask: Option<Mask>,
 }
 
+fn apply_mask_v2(addr: u64, floating: &[u64]) -> Vec<u64> {
+    if floating.is_empty() {
+        return vec![];
+    }
+    let mask = floating[0];
+    let floating = &floating[1..];
+    let on = addr | mask;
+    let off = addr & !mask;
+    [
+        vec![on, off],
+        apply_mask_v2(on, floating),
+        apply_mask_v2(off, floating),
+    ]
+    .iter()
+    .flatten()
+    .copied()
+    .collect()
+}
+
 impl Computer {
     fn new(instructions: Vec<Instr>) -> Self {
         Computer {
@@ -66,18 +95,49 @@ impl Computer {
         }
     }
 
-    fn apply_mask(&self, val: u64) -> u64 {
+    fn apply_mask_v1(&self, val: u64) -> u64 {
         self.mask.map(|m| val & m.and | m.or).unwrap_or(val)
     }
 
-    fn tick(&mut self) -> Option<()> {
+    fn tick_v1(&mut self) -> Option<()> {
         if self.instr_idx >= self.instructions.len() {
             return None;
         }
         match self.instructions[self.instr_idx] {
             Instr::Mask(mask) => self.mask = Some(mask),
             Instr::Mem(addr, val) => {
-                self.state.insert(addr, self.apply_mask(val));
+                self.state.insert(addr, self.apply_mask_v1(val));
+            }
+        }
+        self.instr_idx += 1;
+        Some(())
+    }
+
+    fn apply_mask_v2(&self, addr: u64) -> Vec<u64> {
+        match self.mask {
+            Some(m) => {
+                let addr = addr | m.or;
+                let bits = (0..64)
+                    .map(|i| 1u64 << i)
+                    .map(|p| p & m.floating_bits)
+                    .filter(|&maybe_bit| maybe_bit != 0)
+                    .collect::<Vec<_>>();
+                apply_mask_v2(addr, &bits)
+            }
+            None => Vec::new(),
+        }
+    }
+
+    fn tick_v2(&mut self) -> Option<()> {
+        if self.instr_idx >= self.instructions.len() {
+            return None;
+        }
+        match self.instructions[self.instr_idx] {
+            Instr::Mask(mask) => self.mask = Some(mask),
+            Instr::Mem(addr, val) => {
+                self.apply_mask_v2(addr).iter().for_each(|addr| {
+                    self.state.insert(*addr, val);
+                });
             }
         }
         self.instr_idx += 1;
@@ -97,19 +157,23 @@ impl FromStr for Computer {
 }
 
 fn part1(computer: &mut Computer) -> u64 {
-    while computer.tick() != None {}
+    while computer.tick_v1() != None {}
     computer.state.values().sum()
 }
 
-fn part2() {}
+fn part2(computer: &mut Computer) -> u64 {
+    while computer.tick_v2() != None {}
+    computer.state.values().sum()
+}
 
 fn main() -> Result<()> {
     let mut computer = read_file("input/day14.txt")?;
     let result = part1(&mut computer);
     println!("part 1: {}", result);
 
-    let result = part2();
-    // println!("part 2: {}", result);
+    let mut computer = read_file("input/day14.txt")?;
+    let result = part2(&mut computer);
+    println!("part 2: {}", result);
     Ok(())
 }
 
@@ -131,6 +195,13 @@ mod tests {
 
     #[test]
     fn test_part2() -> Result<()> {
+        let mut computer = "mask = 000000000000000000000000000000X1001X\n\
+        mem[42] = 100\n\
+        mask = 00000000000000000000000000000000X0XX\n\
+        mem[26] = 1"
+            .parse()?;
+        let result = part2(&mut computer);
+        assert_eq!(result, 208);
         Ok(())
     }
 }
