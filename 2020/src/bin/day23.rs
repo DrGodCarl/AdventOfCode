ef4b9182b2a6b1cbcfe9355e205c00ca59b79183
Carl D. Benson <carl.d.benson@gmail.com>
2020-12-26 01:45:30 -0600

    Day 23 part 2 + redo of part 1. Discovered that a vec where the indices were the cup labels made lookup and manipulation easy and fast.

--- 2020/src/bin/day23.rs
+++ 2020/src/bin/day23.rs
@@ -1,144 +1,73 @@
-#[macro_use]
-extern crate lazy_static;
+type Cup = usize;
 
-use std::{collections::HashMap, num::ParseIntError, str::FromStr};
-
-use anyhow::Result;
-use itertools::Itertools;
-use regex::Regex;
-use utils::{read_file, InputParseError};
-
-struct Cups {
-    cups: Vec<u8>,
-    current_cup: u8,
-    picked_cups: Vec<u8>,
-    max: u8,
+fn parse(s: &str) -> Vec<Cup> {
+    s.chars().map(|c| c.to_digit(10).unwrap() as Cup).collect()
 }
 
-impl FromStr for Cups {
-    type Err = InputParseError;
+fn play(cups: &[Cup], times: usize) -> Vec<Cup> {
+    let max = *cups.iter().max().unwrap();
+    let length = cups.len();
+    let mut cup_to_neighbor = vec![0; max + 1];
+    (0..length).for_each(|i| cup_to_neighbor[cups[i]] = cups[(i + 1) % length]);
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        s.chars()
-            .map(|c| c.to_string().parse())
-            .collect::<Result<_, _>>()
-            .map_err(|_| InputParseError)
-            .map(Cups::new)
-    }
-}
-
-impl Cups {
-    fn new(cups: Vec<u8>) -> Self {
-        let current_cup = cups[0];
-        let max = *cups.iter().max().unwrap();
-        Cups {
-            cups,
-            current_cup,
-            picked_cups: Vec::new(),
-            max,
+    let minus_one = |c: Cup| -> Cup {
+        match c - 1 {
+            0 => max,
+            a => a,
         }
-    }
+    };
+    let mut current_cup = cups[0];
+    for _ in 0..times {
+        let take1 = cup_to_neighbor[current_cup];
+        let take2 = cup_to_neighbor[take1];
+        let take3 = cup_to_neighbor[take2];
+        cup_to_neighbor[current_cup] = cup_to_neighbor[take3];
 
-    fn index_of_cup(&self, cup: u8) -> usize {
-        let (index, _) = self
-            .cups
-            .iter()
-            .enumerate()
-            .find(|(_, &c)| c == cup)
-            .unwrap();
-        index
-    }
-
-    fn index_of_current_cup(&self) -> usize {
-        self.index_of_cup(self.current_cup)
-    }
-
-    fn pick_up(&mut self) {
-        let index = self.index_of_current_cup();
-        let indices_to_pick = (index + 1..=index + 3)
-            .map(|i| i % self.cups.len())
-            .collect::<Vec<_>>();
-        let (cups, picked) = (index..index + self.cups.len())
-            .map(|i| i % self.cups.len())
-            .map(|i| (i, self.cups[i]))
-            .fold((Vec::new(), Vec::new()), |(mut leave, mut pick), (i, c)| {
-                if indices_to_pick.contains(&i) {
-                    pick.push(c);
-                } else {
-                    leave.push(c);
-                }
-                (leave, pick)
-            });
-        self.cups = cups;
-        self.picked_cups = picked;
-    }
-
-    fn choose_destination(&self) -> u8 {
-        let minus_one = |c: u8| -> u8 {
-            match c - 1 {
-                0 => self.max,
-                a => a,
-            }
-        };
-        let mut target = minus_one(self.current_cup);
-        while !self.cups.contains(&target) {
+        let mut target = minus_one(current_cup);
+        while [take1, take2, take3].contains(&target) {
             target = minus_one(target);
         }
-        target
-    }
 
-    fn put_down(&mut self) {
-        let destination = self.choose_destination();
-        let index = self.index_of_cup(destination) + 1;
-        let picked = self.picked_cups.clone();
-        self.picked_cups = Vec::new();
-        self.cups.splice(index..index, picked);
-    }
+        let temp = cup_to_neighbor[target];
+        cup_to_neighbor[target] = take1;
+        cup_to_neighbor[take3] = temp;
 
-    fn adjust_current_cup(&mut self) {
-        let index = (self.index_of_current_cup() + 1) % self.cups.len();
-        self.current_cup = self.cups[index];
+        current_cup = cup_to_neighbor[current_cup];
     }
-
-    fn cups_from_cup(&self, cup: u8) -> Vec<u8> {
-        let index = self.index_of_cup(cup);
-        (index..index + self.cups.len())
-            .map(|i| i % self.cups.len())
-            .map(|i| self.cups[i])
-            .collect()
-    }
-
-    fn tick(&mut self) {
-        self.pick_up();
-        self.put_down();
-        self.adjust_current_cup();
-    }
+    cup_to_neighbor
 }
 
-fn part1(mut cups: Cups) -> String {
-    for _ in 0..100 {
-        cups.tick();
+fn part1(cups: &[Cup]) -> String {
+    let cup_to_neighbor = play(cups, 100);
+    let mut res = String::new();
+    let mut cup = 1;
+    while cup_to_neighbor[cup] != 1 {
+        let next_cup = cup_to_neighbor[cup];
+        res.push_str(&next_cup.to_string());
+        cup = next_cup;
     }
-    cups.cups_from_cup(1)
+    res
+}
+
+fn part2(initial_cups: &[Cup]) -> usize {
+    let cups = initial_cups
         .iter()
-        .skip(1)
-        .map(|&i| i.to_string())
-        .join("")
+        .copied()
+        .chain(10usize..=1_000_000)
+        .collect::<Vec<_>>();
+    let cup_to_neighbor = play(&cups, 10_000_000);
+    let after_one = cup_to_neighbor[1];
+    let after_that = cup_to_neighbor[after_one];
+    after_one * after_that
 }
 
-fn part2(mut cups: Cups) -> String {
-    String::default()
-}
-
-fn main() -> Result<()> {
-    let cups = "318946572".parse()?;
-    let result = part1(cups);
+fn main() {
+    let cups = parse("318946572");
+    let result = part1(&cups);
     println!("part 1: {}", result);
 
-    let cups = "318946572".parse()?;
-    let result = part2(cups);
+    let result = part2(&cups);
     println!("part 2: {}", result);
-    Ok(())
 }
 
 #[cfg(test)]
@@ -146,18 +75,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_part1() -> Result<()> {
-        let cups = "389125467".parse()?;
-        let result = part1(cups);
+    fn test_part1() {
+        let cups = parse("389125467");
+        let result = part1(&cups);
         assert_eq!(result, "67384529");
-        Ok(())
     }
 
-    // #[test]
-    // fn test_part2() -> Result<()> {
-    // let cups = "389125467".parse()?;
-    // let result = part1(cups);
-    // assert_eq!(result, "67384529");
-    // Ok(())
-    // }
+    #[test]
+    fn test_part2() {
+        let cups = parse("389125467");
+        let result = part2(&cups);
+        assert_eq!(result, 149245887792);
+    }
 }
