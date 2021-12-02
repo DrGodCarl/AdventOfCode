2488a9f05b8b61687a4dff91b196315b5aaee886
Carl D. Benson <carl.d.benson@gmail.com>
2021-12-02 09:09:58 -0600

    Day 2, 2021. Cleanup now that I know about parse_displays true power.

--- 2021/src/bin/day02.rs
+++ 2021/src/bin/day02.rs
@@ -2,30 +2,31 @@ use anyhow::Result;
 use utils::read_lines;
 
 #[derive(parse_display::FromStr, PartialEq, Debug, Clone, Copy)]
+#[display("{} {0}")]
+#[display(style = "snake_case")]
 enum Instr {
-    #[display("forward {0}")]
     Forward(isize),
-    #[display("up {0}")]
     Up(isize),
-    #[display("down {0}")]
     Down(isize),
 }
 
 fn part1(numbers: &[Instr]) -> isize {
-    let point = numbers.iter().fold((0, 0), |acc, instr| match instr {
-        Instr::Forward(x) => (acc.0 + x, acc.1),
-        Instr::Up(y) => (acc.0, acc.1 - y),
-        Instr::Down(y) => (acc.0, acc.1 + y),
+    let point = numbers.iter().fold((0, 0), |(x, y), instr| match instr {
+        Instr::Forward(n) => (x + n, y),
+        Instr::Up(n) => (x, y - n),
+        Instr::Down(n) => (x, y + n),
     });
     point.0 * point.1
 }
 
 fn part2(numbers: &[Instr]) -> isize {
-    let point = numbers.iter().fold((0, 0, 0), |acc, instr| match instr {
-        Instr::Forward(x) => (acc.0 + x, acc.1 + acc.2 * x, acc.2),
-        Instr::Up(y) => (acc.0, acc.1, acc.2 - y),
-        Instr::Down(y) => (acc.0, acc.1, acc.2 + y),
-    });
+    let point = numbers
+        .iter()
+        .fold((0, 0, 0), |(x, y, aim), instr| match instr {
+            Instr::Forward(n) => (x + n, y + aim * n, aim),
+            Instr::Up(n) => (x, y, aim - n),
+            Instr::Down(n) => (x, y, aim + n),
+        });
     point.0 * point.1
 }
 
