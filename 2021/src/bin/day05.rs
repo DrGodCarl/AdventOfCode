c92e4197c59641ecdc293d2db1d7e7d8944ab06a
Carl D. Benson <carl.d.benson@gmail.com>
2021-12-05 21:14:59 -0600

    Day 5, 2021. I over-engineered this, but being able to calculate all lattice points on a line segment is probably useful.

---
+++ 2021/src/bin/day05.rs
@@ -0,0 +1,169 @@
+use gcd::Gcd;
+use std::collections::{HashMap, HashSet};
+
+use anyhow::Result;
+use utils::read_lines;
+
+#[derive(parse_display::FromStr, PartialEq, Eq, Hash, Debug, Clone, Copy)]
+#[display("{0},{1}")]
+struct Point(isize, isize);
+
+#[derive(parse_display::FromStr, PartialEq, Debug, Clone, Copy)]
+#[display("{start} -> {end}")]
+struct Line {
+    start: Point,
+    end: Point,
+}
+
+fn lattice_points(line: &Line) -> HashSet<Point> {
+    if line.start == line.end {
+        // The line is really a point.
+        let mut result = HashSet::new();
+        result.insert(line.start);
+        return result;
+    }
+    // Figure out the slope as a reduced fraction (rise/run)
+    let run = line.end.0 - line.start.0;
+    let rise = line.end.1 - line.start.1;
+    let factor = (rise.abs() as usize).gcd(run.abs() as usize) as isize;
+    let run = run / factor;
+    let rise = rise / factor;
+    // If the x coord is going down, adjust for that
+    let run_range: Box<dyn Iterator<Item = isize>> = if run > 0 {
+        Box::new(line.start.0..=line.end.0)
+    } else {
+        Box::new((line.end.0..=line.start.0).rev())
+    };
+    // If the y coord is going down, adjust for that
+    let rise_range: Box<dyn Iterator<Item = isize>> = if rise > 0 {
+        Box::new(line.start.1..=line.end.1)
+    } else {
+        Box::new((line.end.1..=line.start.1).rev())
+    };
+    if run == 0 {
+        // If the x coord is unchanging, just hit every int between y start and end
+        rise_range
+            .step_by(rise.abs() as usize)
+            .map(|y| Point(line.start.0, y))
+            .collect()
+    } else if rise == 0 {
+        // If the y coord is unchanging, just hit every int between z start and end
+        run_range
+            .step_by(run.abs() as usize)
+            .map(|x| Point(x, line.start.1))
+            .collect()
+    } else {
+        // Otherwise, in the general case, zip up the x range stepped by the run rate
+        // and the y range stepped by the rise rate, and those are the lattice points.
+        run_range
+            .step_by(run.abs() as usize)
+            .zip(rise_range.step_by(rise.abs() as usize))
+            .map(|(x, y)| Point(x, y))
+            .collect()
+    }
+}
+
+fn count_overlapping_points(lines: &[Line]) -> usize {
+    lines
+        .iter()
+        .flat_map(|l| lattice_points(l))
+        .fold(HashMap::new(), |mut map, p| {
+            *map.entry(p).or_insert(0) += 1;
+            map
+        })
+        .iter()
+        .filter(|(_, &c)| c > 1)
+        .count()
+}
+
+fn part1(lines: &[Line]) -> usize {
+    let lines: Vec<Line> = lines
+        .iter()
+        .filter(|l| l.start.0 == l.end.0 || l.start.1 == l.end.1)
+        .copied()
+        .collect();
+
+    count_overlapping_points(&lines)
+}
+
+fn part2(lines: &[Line]) -> usize {
+    count_overlapping_points(lines)
+}
+
+fn main() -> Result<()> {
+    let numbers = read_lines("input/day05.txt")?;
+    let result = part1(&numbers);
+    println!("part 1: {}", result);
+    let result = part2(&numbers);
+    println!("part 2: {}", result);
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parts() -> Result<()> {
+        let numbers = read_lines("input/test/day05.txt")?;
+        let result = part1(&numbers);
+        assert_eq!(result, 5);
+
+        let result = part2(&numbers);
+        assert_eq!(result, 12);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_lattice_points() -> Result<()> {
+        let line = "0,0 -> 1,1".parse()?;
+        let result = lattice_points(&line);
+        assert!(result.contains(&Point(0, 0)));
+        assert!(result.contains(&Point(1, 1)));
+        assert_eq!(result.len(), 2);
+
+        let line = "1,1 -> 3,3".parse()?;
+        let result = lattice_points(&line);
+        assert!(result.contains(&Point(1, 1)));
+        assert!(result.contains(&Point(2, 2)));
+        assert!(result.contains(&Point(3, 3)));
+        assert_eq!(result.len(), 3);
+
+        let line = "0,0 -> 2,4".parse()?;
+        let result = lattice_points(&line);
+        assert!(result.contains(&Point(0, 0)));
+        assert!(result.contains(&Point(1, 2)));
+        assert!(result.contains(&Point(2, 4)));
+        assert_eq!(result.len(), 3);
+
+        let line = "2,4 -> 0,0".parse()?;
+        let result = lattice_points(&line);
+        assert!(result.contains(&Point(0, 0)));
+        assert!(result.contains(&Point(1, 2)));
+        assert!(result.contains(&Point(2, 4)));
+        assert_eq!(result.len(), 3);
+
+        let line = "1,1 -> 1,4".parse()?;
+        let result = lattice_points(&line);
+        assert!(result.contains(&Point(1, 1)));
+        assert!(result.contains(&Point(1, 2)));
+        assert!(result.contains(&Point(1, 3)));
+        assert!(result.contains(&Point(1, 4)));
+        assert_eq!(result.len(), 4);
+
+        let line = "1,1 -> 4,1".parse()?;
+        let result = lattice_points(&line);
+        assert!(result.contains(&Point(1, 1)));
+        assert!(result.contains(&Point(2, 1)));
+        assert!(result.contains(&Point(3, 1)));
+        assert!(result.contains(&Point(4, 1)));
+        assert_eq!(result.len(), 4);
+
+        let line = "1,1 -> 1,1".parse()?;
+        let result = lattice_points(&line);
+        assert!(result.contains(&Point(1, 1)));
+        assert_eq!(result.len(), 1);
+        Ok(())
+    }
+}
