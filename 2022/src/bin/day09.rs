dc6b235db6d0f8887dbf7a290f25ee7836801c77
Carl Benson <carl.d.benson@gmail.com>
2022-12-09 00:31:03 -0600

    Day 09, 2022. That diagonal movement was tricky. The text even warned me there was weird motion and I just didn't see it.

---
+++ 2022/src/bin/day09.rs
@@ -0,0 +1,105 @@
+use std::collections::HashSet;
+
+use anyhow::Result;
+use itertools::Itertools;
+use parse_display::FromStr;
+use utils::read_lines;
+
+#[derive(FromStr, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[display("{direction} {distance}")]
+struct Instruction {
+    direction: Direction,
+    distance: u8,
+}
+
+#[derive(FromStr, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+enum Direction {
+    #[display("U")]
+    Up,
+    #[display("D")]
+    Down,
+    #[display("L")]
+    Left,
+    #[display("R")]
+    Right,
+}
+
+fn run(instructions: &[Instruction], rope_size: u8) -> u32 {
+    let rope = (0..rope_size - 1).map(|_| (0, 0)).collect_vec();
+    let mut initial_tail_pos = HashSet::new();
+    initial_tail_pos.insert((0, 0));
+    instructions
+        .iter()
+        .flat_map(|i| (0..i.distance).map(|_| i.direction))
+        .fold(
+            (initial_tail_pos, rope, (0, 0)),
+            |(mut tail_pos, cur_rope, h_pos), d| {
+                let new_h_pos = match d {
+                    Direction::Up => (h_pos.0, h_pos.1 + 1),
+                    Direction::Down => (h_pos.0, h_pos.1 - 1),
+                    Direction::Left => (h_pos.0 - 1, h_pos.1),
+                    Direction::Right => (h_pos.0 + 1, h_pos.1),
+                };
+                let (_, rest_of_rope) = cur_rope.iter().fold(
+                    (new_h_pos, Vec::new()),
+                    |(effective_h_pos, mut new_rope), t_pos| {
+                        let x_diagonal_detect = (effective_h_pos.0 - t_pos.0) / 2;
+                        let y_diagonal_detect = (effective_h_pos.1 - t_pos.1) / 2;
+                        let new_t_pos = if x_diagonal_detect != 0 && y_diagonal_detect != 0 {
+                            (t_pos.0 + x_diagonal_detect, t_pos.1 + y_diagonal_detect)
+                        } else if t_pos.0 < effective_h_pos.0 - 1 {
+                            (effective_h_pos.0 - 1, effective_h_pos.1)
+                        } else if t_pos.0 > effective_h_pos.0 + 1 {
+                            (effective_h_pos.0 + 1, effective_h_pos.1)
+                        } else if t_pos.1 < effective_h_pos.1 - 1 {
+                            (effective_h_pos.0, effective_h_pos.1 - 1)
+                        } else if t_pos.1 > effective_h_pos.1 + 1 {
+                            (effective_h_pos.0, effective_h_pos.1 + 1)
+                        } else {
+                            (t_pos.0, t_pos.1)
+                        };
+                        new_rope.push(new_t_pos);
+                        (new_t_pos, new_rope)
+                    },
+                );
+                tail_pos.insert(rest_of_rope.last().unwrap().clone());
+                (tail_pos, rest_of_rope, new_h_pos)
+            },
+        )
+        .0
+        .len() as u32
+}
+
+fn part1(instructions: &[Instruction]) -> u32 {
+    run(instructions, 2)
+}
+
+fn part2(instructions: &[Instruction]) -> u32 {
+    run(instructions, 10)
+}
+
+fn main() -> Result<()> {
+    let instructions = read_lines("input/day09.txt")?;
+    let result = part1(&instructions);
+    println!("part 1: {}", result);
+    let result = part2(&instructions);
+    println!("part 2: {}", result);
+    Ok(())
+}
+
+#[test]
+fn test() -> Result<()> {
+    let instructions = read_lines("input/test/day09.txt")?;
+    let result = part1(&instructions);
+    assert_eq!(result, 13);
+
+    let instructions = read_lines("input/test/day09.txt")?;
+    let result = part2(&instructions);
+    assert_eq!(result, 1);
+
+    let instructions = read_lines("input/test/day09_2.txt")?;
+    let result = part2(&instructions);
+    assert_eq!(result, 36);
+
+    Ok(())
+}
