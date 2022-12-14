e4829689a5686cafab0d5d551ec7795a35e9239c
Carl Benson <carl.d.benson@gmail.com>
2022-12-14 10:45:26 -0600

    Day 14, 2022. Refactored now that I'm in my right mind. Many, MANY times faster. Like 1000 times faster at least.

--- 2022/src/bin/day14.rs
+++ 2022/src/bin/day14.rs
@@ -1,28 +1,10 @@
-use std::{collections::HashSet, ops::RangeInclusive, str::FromStr};
+use std::{collections::HashSet, str::FromStr};
 
 use anyhow::Result;
+use itertools::Itertools;
 use utils::{read_file, InputParseError};
 
-#[derive(Clone, Debug)]
-enum PointRange {
-    XRange(RangeInclusive<i32>, i32),
-    YRange(i32, RangeInclusive<i32>),
-}
-
-impl PointRange {
-    fn contains(&self, (x, y): &(i32, i32)) -> bool {
-        match self {
-            PointRange::XRange(range, y2) => range.contains(&x) && y == y2,
-            PointRange::YRange(x2, range) => range.contains(&y) && x == x2,
-        }
-    }
-}
-
-// Parse strings to create PointRanges. String looks like:
-// 498,4 -> 498,6 -> 496,6
-// Output for this would a PointRange::YRange(498, 4..=6) and a PointRange::XRange(496..=498, 6)
-fn parse_point_ranges(s: &str) -> Result<Vec<PointRange>> {
-    let mut ranges = Vec::new();
+fn parse_point_ranges(s: &str) -> Result<HashSet<(i32, i32)>> {
     let points = s
         .split(" -> ")
         .map(|p| {
@@ -33,127 +15,132 @@ fn parse_point_ranges(s: &str) -> Result<Vec<PointRange>> {
         })
         .collect::<Result<Vec<(i32, i32)>>>()?;
 
-    let mut prev = points[0];
-    for point in points.iter().skip(1) {
-        if point.0 == prev.0 {
-            ranges.push(PointRange::YRange(
-                point.0,
-                (prev.1.min(point.1))..=(prev.1.max(point.1)),
-            ));
-        } else {
-            ranges.push(PointRange::XRange(
-                (prev.0.min(point.0))..=(prev.0.max(point.0)),
-                point.1,
-            ));
-        }
-        prev = point.clone();
-    }
-    Ok(ranges)
+    Ok(points
+        .iter()
+        .tuple_windows()
+        .flat_map(|(s, e)| {
+            (s.0.min(e.0)..=s.0.max(e.0)).cartesian_product(s.1.min(e.1)..=s.1.max(e.1))
+        })
+        .collect())
 }
 
 #[derive(Clone, Debug)]
-struct CrossSectionMap<const P2: bool> {
-    ranges: Vec<PointRange>,
-    sand_origin: (i32, i32),
-    sand_point: (i32, i32),
+struct CrossSectionMap {
+    rock: HashSet<(i32, i32)>,
     sand_pile: HashSet<(i32, i32)>,
     max_y: i32,
+    sand_path: Vec<(i32, i32)>,
+    floor: Option<i32>,
 }
 
-impl<const P2: bool> CrossSectionMap<P2> {
-    fn new(ranges: Vec<PointRange>, sand_origin: (i32, i32)) -> Self {
-        let max_y = ranges
-            .iter()
-            .map(|r| match r {
-                PointRange::XRange(_, y) => *y,
-                PointRange::YRange(_, ys) => *ys.start(),
-            })
-            .max()
-            .unwrap()
-            .clone()
-            + if P2 { 2 } else { 0 };
+impl CrossSectionMap {
+    fn new(rock: HashSet<(i32, i32)>, sand_origin: (i32, i32)) -> Self {
+        let max_y = rock.iter().map(|p| p.1).max().unwrap_or(0);
         Self {
-            ranges,
-            sand_origin,
-            sand_point: sand_origin,
+            rock,
             sand_pile: HashSet::new(),
             max_y,
+            sand_path: vec![sand_origin],
+            floor: None,
         }
     }
 
-    fn next_point_falling(&self, point: &(i32, i32)) -> (i32, i32) {
-        let (x, y) = point;
-        let mut next_point = (*x, y + 1);
-        if self.collides(&next_point) {
-            next_point = (x - 1, y + 1);
-        }
-        if self.collides(&next_point) {
-            next_point = (x + 1, y + 1);
-        }
-        if self.collides(&next_point) {
-            next_point = point.clone();
-        }
-        next_point
+    fn add_floor(&mut self, floor_distance: i32) {
+        self.max_y = self.max_y + floor_distance;
+        self.floor = Some(self.max_y);
     }
 
-    fn collides(&self, point: &(i32, i32)) -> bool {
-        (point.1 == self.max_y && P2)
-            || self.sand_pile.contains(point)
-            || self.ranges.iter().any(|r| r.contains(point))
-    }
+    #[allow(dead_code)]
+    fn draw(&self) {
+        let min_x = self.rock.iter().map(|p| p.0).min().unwrap_or(0) - 2;
+        let max_x = self.rock.iter().map(|p| p.0).max().unwrap_or(0) + 2;
+        let min_y = 0;
+        let max_y = self.rock.iter().map(|p| p.1).max().unwrap_or(0) + 2;
 
-    fn step_sand(&mut self) -> bool {
-        let next_point = self.next_point_falling(&self.sand_point);
-
-        if next_point.1 > self.max_y {
-            return false;
+        for y in min_y..=max_y {
+            for x in min_x..=max_x {
+                let point = (x, y);
+                if self.rock.contains(&point) {
+                    print!("#");
+                } else if self.sand_pile.contains(&point) {
+                    print!("o");
+                } else if self.floor == Some(y) {
+                    print!("=");
+                } else {
+                    print!(".");
+                }
+            }
+            println!();
         }
-
-        if next_point == self.sand_origin {
-            self.sand_pile.insert(self.sand_point);
-            return false;
-        }
-
-        if next_point == self.sand_point {
-            self.sand_pile.insert(self.sand_point);
-            self.sand_point = self.sand_origin;
-        } else {
-            self.sand_point = next_point;
-        }
-
-        true
     }
 }
 
-impl<const P2: bool> FromStr for CrossSectionMap<P2> {
+impl FromStr for CrossSectionMap {
     type Err = InputParseError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let ranges = s
+        let rock = s
             .lines()
             .filter_map(|l| parse_point_ranges(l).ok())
             .flatten()
             .collect();
 
-        Ok(Self::new(ranges, (500, 0)))
+        Ok(Self::new(rock, (500, 0)))
     }
 }
 
-fn part1(map: &mut CrossSectionMap<false>) -> u32 {
-    while map.step_sand() {}
+fn has_next_step(map: &CrossSectionMap) -> bool {
+    map.sand_path.len() > 0
+        && map
+            .sand_path
+            .last()
+            .map(|p| p.1 < map.max_y)
+            .unwrap_or(false)
+}
+
+fn collides(map: &CrossSectionMap, point: &(i32, i32)) -> bool {
+    map.rock.contains(&point) || map.sand_pile.contains(&point) || map.floor == Some(point.1)
+}
+
+fn take_next_step(map: &mut CrossSectionMap) {
+    let sand_point = map.sand_path.pop().unwrap();
+    let mut next_point = (sand_point.0, sand_point.1 + 1);
+    if collides(map, &next_point) {
+        next_point = (sand_point.0 - 1, sand_point.1 + 1);
+    }
+    if collides(map, &next_point) {
+        next_point = (sand_point.0 + 1, sand_point.1 + 1);
+    }
+    if collides(map, &next_point) {
+        next_point = sand_point;
+    }
+    if sand_point == next_point {
+        map.sand_pile.insert(sand_point);
+    } else {
+        map.sand_path.push(sand_point);
+        map.sand_path.push(next_point);
+    }
+}
+
+fn part1(map: &mut CrossSectionMap) -> u32 {
+    while has_next_step(map) {
+        take_next_step(map);
+    }
     map.sand_pile.len() as u32
 }
 
-fn part2(map: &mut CrossSectionMap<true>) -> u32 {
-    while map.step_sand() {}
+fn part2(map: &mut CrossSectionMap) -> u32 {
+    map.add_floor(2);
+    while has_next_step(map) {
+        take_next_step(map);
+    }
     map.sand_pile.len() as u32
 }
 
 fn main() -> Result<()> {
-    let map: CrossSectionMap<false> = read_file("input/day14.txt")?;
+    let map: CrossSectionMap = read_file("input/day14.txt")?;
     let result = part1(&mut map.clone());
     println!("part 1: {}", result);
-    let map: CrossSectionMap<true> = read_file("input/day14.txt")?;
     let result = part2(&mut map.clone());
     println!("part 2: {}", result);
     Ok(())
@@ -161,11 +148,10 @@ fn main() -> Result<()> {
 
 #[test]
 fn test() -> Result<()> {
-    let map: CrossSectionMap<false> = read_file("input/test/day14.txt")?;
+    let map: CrossSectionMap = read_file("input/test/day14.txt")?;
     let result = part1(&mut map.clone());
     assert_eq!(result, 24);
 
-    let map: CrossSectionMap<true> = read_file("input/test/day14.txt")?;
     let result = part2(&mut map.clone());
     assert_eq!(result, 93);
 
